26967755fbecd9b7bad0f954aa7ead6ae1e895ab
wayne-dooley <42575447+wayne-dooley@users.noreply.github.com>
2021-04-21 14:40:13 -0500

    Update ttw.go

--- ttw.go
+++ ttw.go
@@ -1,8 +1,8 @@
 /* Tic-Tac-Wayno
-*  
+*
 *  My very first Go language program
 *
-*/
+ */
 
 package main
 
@@ -11,7 +11,7 @@ import (
 )
 
 // Displays the playing board
-func showBoard (sb [10] string) {
+func showBoard(sb [10]string) {
 	fmt.Println("\n  ", sb[1], "|", sb[2], "|", sb[3])
 	fmt.Println("  -----------")
 	fmt.Println("  ", sb[4], "|", sb[5], "|", sb[6])
@@ -20,13 +20,13 @@ func showBoard (sb [10] string) {
 }
 
 // Display into
-func main () {
+func main() {
 
 	// Intro screen
-	// 
+	//
 	fmt.Println("\n*********************************")
 	fmt.Println("**                             **")
-	fmt.Println("**  Welcome to tic-tac-wayno!  **")
+	fmt.Println("**  Welcome to tic-tac-wayno!!!  **")
 	fmt.Println("**                             **")
 	fmt.Println("*********************************\n")
 
@@ -42,11 +42,11 @@ func main () {
 	*    " " = available move
 	*    "X" = user move (X)
 	*    "O" = computer move (O)
-	*/
+	 */
 
 	// Initialize the board
-	board := [...]string{""," "," "," "," "," "," "," "," "," "}
-	
+	board := [...]string{"", " ", " ", " ", " ", " ", " ", " ", " ", " "}
+
 	// Refresh the display
 	showBoard(board)
 
@@ -55,31 +55,31 @@ func main () {
 	var tie bool
 	var xWin bool
 
-	var corner = [4]int{1, 3, 7, 9}  // Define corners
-	var side = [4]int{2, 4, 6, 8}    // Define sides
+	var corner = [4]int{1, 3, 7, 9} // Define corners
+	var side = [4]int{2, 4, 6, 8}   // Define sides
 
-	// Declare all possible board winning positions 
+	// Declare all possible board winning positions
 	var b = [8][3]int{
 		// diagonal
-		{1, 5, 9} ,
-		{3, 5, 7} ,
+		{1, 5, 9},
+		{3, 5, 7},
 		// horizontal
-		{1, 2, 3} ,
-		{4, 5, 6} ,
-		{7, 8, 9} ,
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
 		// vertical
-		{1, 4, 7} ,
-		{2, 5, 8} ,
-		{3, 6, 9} }
+		{1, 4, 7},
+		{2, 5, 8},
+		{3, 6, 9}}
 
 	// Start the game loop
 	for {
-	
+
 		/*
 		**
 		**  User move (X)
 		**
-		*/
+		 */
 
 		// Accept user move (X) and validate
 		fmt.Print("Player X move : ")
@@ -87,7 +87,7 @@ func main () {
 
 		for {
 			// Must be 1-9, and square must be available
-			if (xMove < 1 || xMove > 9) {
+			if xMove < 1 || xMove > 9 {
 				fmt.Print("\n** Must be between 1 and 9, try again : ")
 				fmt.Scanf("%d ", &xMove)
 			} else if board[xMove] != " " {
@@ -104,7 +104,7 @@ func main () {
 		// Did X win?
 		xWin = false
 		for i := 0; i < 8; i++ {
-			if (board[b[i][0]] == "X" && board[b[i][1]] == "X" && board[b[i][2]] == "X") {
+			if board[b[i][0]] == "X" && board[b[i][1]] == "X" && board[b[i][2]] == "X" {
 				xWin = true
 				break
 			}
@@ -119,7 +119,6 @@ func main () {
 			fmt.Println("***************\n")
 			break
 		}
-
 
 		// Tie game?
 		tie = true
@@ -144,24 +143,24 @@ func main () {
 		**
 		**  Computer move (O)
 		**
-		*/
+		 */
 
 		oTurn = true
 
 		// Can O make a winning move?
 		if oTurn {
 			for i := 0; i < 8; i++ {
-				if (board[b[i][0]] == "O" && board[b[i][1]] == "O" && board[b[i][2]] == " ") {
+				if board[b[i][0]] == "O" && board[b[i][1]] == "O" && board[b[i][2]] == " " {
 					board[b[i][2]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == "O" && board[b[i][1]] == " " && board[b[i][2]] == "O") {
+				if board[b[i][0]] == "O" && board[b[i][1]] == " " && board[b[i][2]] == "O" {
 					board[b[i][1]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == " " && board[b[i][1]] == "O" && board[b[i][2]] == "O") {
+				if board[b[i][0]] == " " && board[b[i][1]] == "O" && board[b[i][2]] == "O" {
 					board[b[i][0]] = "O"
 					oTurn = false
 					break
@@ -172,17 +171,17 @@ func main () {
 		// Does O need a simple block?
 		if oTurn {
 			for i := 0; i < 8; i++ {
-				if (board[b[i][0]] == "X" && board[b[i][1]] == "X" && board[b[i][2]] == " ") {
+				if board[b[i][0]] == "X" && board[b[i][1]] == "X" && board[b[i][2]] == " " {
 					board[b[i][2]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == "X" && board[b[i][1]] == " " && board[b[i][2]] == "X") {
+				if board[b[i][0]] == "X" && board[b[i][1]] == " " && board[b[i][2]] == "X" {
 					board[b[i][1]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == " " && board[b[i][1]] == "X" && board[b[i][2]] == "X") {
+				if board[b[i][0]] == " " && board[b[i][1]] == "X" && board[b[i][2]] == "X" {
 					board[b[i][0]] = "O"
 					oTurn = false
 					break
@@ -193,7 +192,7 @@ func main () {
 		// Does O need a corner block?
 		if oTurn {
 			if (board[1] == "X" && board[9] == "X" && board[5] == "O") ||
-			   (board[3] == "X" && board[7] == "X" && board[5] == "O") {
+				(board[3] == "X" && board[7] == "X" && board[5] == "O") {
 				for i := 0; i < 3; i++ {
 					if board[side[i]] == " " {
 						board[side[i]] = "O"
@@ -201,7 +200,7 @@ func main () {
 						break
 					}
 				}
-	
+
 			}
 
 		}
@@ -209,17 +208,17 @@ func main () {
 		// Can O set up for a win?
 		if oTurn {
 			for i := 0; i < 8; i++ {
-				if (board[b[i][0]] == "O" && board[b[i][1]] == " " && board[b[i][2]] == " ") {
+				if board[b[i][0]] == "O" && board[b[i][1]] == " " && board[b[i][2]] == " " {
 					board[b[i][1]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == " " && board[b[i][1]] == "O" && board[b[i][2]] == " ") {
+				if board[b[i][0]] == " " && board[b[i][1]] == "O" && board[b[i][2]] == " " {
 					board[b[i][0]] = "O"
 					oTurn = false
 					break
 				}
-				if (board[b[i][0]] == " " && board[b[i][1]] == " " && board[b[i][2]] == "0") {
+				if board[b[i][0]] == " " && board[b[i][1]] == " " && board[b[i][2]] == "0" {
 					board[b[i][1]] = "O"
 					oTurn = false
 					break
@@ -248,31 +247,31 @@ func main () {
 
 		// Make a random O move
 		if oTurn {
-			for i := 1; i <=9; i++ {
+			for i := 1; i <= 9; i++ {
 				if board[i] == " " {
 					board[i] = "O"
 					oTurn = false
 					break
 				}
-			}	
+			}
 		}
 
 		// Did O win?
-		if  (board[1] == "O" && board[2] == "O" && board[3] == "O") || 
-			(board[4] == "O" && board[5] == "O" && board[6] == "O") || 
-			(board[7] == "O" && board[8] == "O" && board[9] == "O") || 
+		if (board[1] == "O" && board[2] == "O" && board[3] == "O") ||
+			(board[4] == "O" && board[5] == "O" && board[6] == "O") ||
+			(board[7] == "O" && board[8] == "O" && board[9] == "O") ||
 			(board[1] == "O" && board[4] == "O" && board[7] == "O") ||
-			(board[2] == "O" && board[5] == "O" && board[8] == "O") || 
-			(board[3] == "O" && board[6] == "O" && board[9] == "O") || 
-			(board[1] == "O" && board[5] == "O" && board[9] == "O") || 
-			(board[3] == "O" && board[5] == "O" && board[7] == "O")    {
-				showBoard(board)
-				fmt.Println("***************")
-				fmt.Println("**           **")
-				fmt.Println("**  O WINS!  **")
-				fmt.Println("**           **")
-				fmt.Println("***************\n")
-				break
+			(board[2] == "O" && board[5] == "O" && board[8] == "O") ||
+			(board[3] == "O" && board[6] == "O" && board[9] == "O") ||
+			(board[1] == "O" && board[5] == "O" && board[9] == "O") ||
+			(board[3] == "O" && board[5] == "O" && board[7] == "O") {
+			showBoard(board)
+			fmt.Println("***************")
+			fmt.Println("**           **")
+			fmt.Println("**  O WINS!  **")
+			fmt.Println("**           **")
+			fmt.Println("***************\n")
+			break
 		}
 
 		// Refresh board
